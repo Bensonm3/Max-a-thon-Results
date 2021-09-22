0eb036d1913cef33a2be5f243b76f2711d5602e7
Matt Benson <rabenson6@msn.com>
2021-09-22 08:00:29 -0700

    updating server and package.json

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use('/athletes', athleteRoute)
 
 // PORT
 const port = process.env.PORT || 4000;
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log('Connected to port ' + port)
 })
 
